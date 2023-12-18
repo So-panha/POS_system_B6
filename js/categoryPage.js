68eba619b834a16bc743052edb75bc048db97d77
CHANNICH.NOEURN <CHANNICH.NOEURN@PNC.LAN>
2023-12-18 11:27:37 +0700

    #20 feature_edit_category

--- js/categoryPage.js
+++ js/categoryPage.js
@@ -1,5 +1,20 @@
 // Set up input 
 let dataCategory = [];
+let indexUpdate = [];
+let search = document.getElementById('search');
+let nameInput = document.getElementById('name');
+let descriptInput = document.getElementById('descript');
+let nameInputUpdate = document.getElementById('nameUpdate');
+let descriptInputUpdate = document.getElementById('descriptUpdate');
+let btnAdd = document.querySelector('.add');
+let btnSave = document.querySelector('.save');
+let btnConcel = document.querySelector('.cancel');
+let btnSaveUpdate = document.querySelector('.saveUpdate');
+let btnConcelUpdate = document.querySelector('.cancelUpdate');
+let table = document.querySelector('table');
+let form = document.querySelector('.form_fill');
+let update = document.querySelector('.form_update');
+
 
 // Save data of storage
 function saveDataStorage() {
@@ -14,6 +29,61 @@ function getDataStorage() {
         createtable()
     }
 }
+
+// Create object
+function createObject() {
+
+    // Call funtion checkInput
+    let limitChar = limitName();
+    let limitScript = limitDescription();
+    if (checkInput(nameInput.value) && limitChar && limitScript) {
+        mainCategory = {};
+        mainCategory.category = nameInput.value;
+        mainCategory.desription = descriptInput.value;
+        dataCategory.push(mainCategory);
+        saveDataStorage();
+        createtable();
+        // close from create category
+        form.style.display = "none";
+
+    } else if (limitChar == false) {
+        alert('You are only allow to input name catagory less than 20!');
+    } else if (checkInput(nameInput.value) === false) {
+        alert('You need to input your category first!');
+    } else if (limitScript == false) {
+        alert('You are only allow to write description less than 20!');
+    }
+}
+
+// Check value of input
+function checkInput(element) {
+    if (element != '') {
+        return true
+    } else {
+        return false
+    }
+}
+
+// limit Charactor in input name
+function limitName() {
+    let lenName = nameInput.value.length;
+    if (lenName < 20) {
+        return true
+    } else {
+        return false
+    }
+}
+
+  // limit Charactor in input description
+  function limitDescription() {
+    let lenDescript = descriptInput.value.length;
+    if (lenDescript < 20) {
+        return true
+    } else {
+        return false
+    }
+}
+
 
 // create list of data
 function createtable() {
@@ -56,25 +126,46 @@ function createtable() {
     }
     // Append tbody into table
     table.appendChild(tbody);
-}
 
 
-// Delect data
-function deleteData(e) {
-    let index = e.target.closest('tr').dataset.index;
-    dataCategory.splice(index, 1);
-    saveDataStorage();
-    createtable();
-}
+    // Delect data
+    function deleteData(e) {
+        let index = e.target.closest('tr').dataset.index;
+        dataCategory.splice(index, 1);
+        saveDataStorage();
+        createtable();
+    }
+
+    // Edit data
+    function editData(e) {
+        // Get index
+        let index = e.target.closest('tr').dataset.index;
+        indexUpdate = index;
+        // Catch value from array object
+        nameInputUpdate.value = dataCategory[index].category;
+        descriptInputUpdate.value = dataCategory[index].desription;
+        // diplay form 
+        form.style.display = "none";
+        update.style.display = "block";
+    }
+
+    // Open form crete category
+    function openForm() {
+        form.style.display = "block";
+    }
+
+    // Close form create category
+    function closeForm() {
+        form.style.display = "none";
+    }
 
 
 
-
-// Call data
-getDataStorage();
-btnAdd.addEventListener('click', openForm);
-btnConcel.addEventListener('click', closeForm);
-btnSave.addEventListener('click', createObject);
-btnSaveUpdate.addEventListener('click', updateForm);
-btnConcelUpdate.addEventListener('click', closeFormUpdate);
-search.addEventListener('input', searchList);
+    // Call data
+    getDataStorage();
+    btnAdd.addEventListener('click', openForm);
+    btnConcel.addEventListener('click', closeForm);
+    btnSave.addEventListener('click', createObject);
+    btnSaveUpdate.addEventListener('click', updateForm);
+    btnConcelUpdate.addEventListener('click', closeFormUpdate);
+    search.addEventListener('input', searchList);
