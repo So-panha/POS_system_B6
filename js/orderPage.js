7117aa5cdbfcf8f8d12b1739387bc78b2080d396
DAVIT.CHOEUN <DAVIT.CHOEUN@pnc.lan>
2023-12-19 12:04:25 +0700

    #42 list_oder

--- js/orderPage.js
+++ js/orderPage.js
@@ -1,15 +1,16 @@
 
 let add = document.querySelector('.click_add')
-let keyword = document.querySelector('h2')
-let totals = document.querySelector('.total');
-let searchId = document.querySelector("#search");
+let tbody = document.querySelector('tbody')
 let inputs = document.querySelectorAll('tr input');
+let totals = document.querySelector('.total');
+let keyword = document.querySelector('h2')
+let searchId = document.querySelector("#search");
 let removeButton = document.querySelectorAll('.delete');
 
 // Set product
+let goods = document.querySelector('.show_list_ID .top');
 let product = document.querySelector('.onder_number');
 let quantity = document.querySelector('.onder_qty');
-let goods = document.querySelector('.show_list_ID .top');
 
 // Create data store
 let dataStore = [];
@@ -33,22 +34,62 @@ function getDataLocalStorage() {
 
 // Show product
 function showProduct(e) {
-    let id = e.target.value; 
+    let id = e.target.value;
     for (let data of dataStore) {
         if (id == data.id) {
             goods.firstElementChild.textContent = data.name;
             quantity.firstElementChild.textContent = 'QTY : ' + data.quantity;
             product.firstElementChild.textContent = data.grossPrice + ' $';
-        }else if(id.length == 0){
-            goods.firstElementChild.textContent =  '';
+        } else if (id.length == 0) {
+            goods.firstElementChild.textContent = '';
             quantity.firstElementChild.textContent = '';
-            product.firstElementChild.textContent =  '';
+            product.firstElementChild.textContent = '';
         }
     }
 }
+
+// show on product
+function showOnProduct() {
+    let tableTR = document.createElement('tr');
+
+    // show id
+    let tdID = document.createElement('td');
+    tdID.textContent = dataStore.id
+
+    // show name
+    let tdName = document.createElement('td');
+    tdName.textContent = dataStore.name
+
+    // show input value 
+    let tdInput = document.createElement('td');
+    let input = document.createElement('input');
+    tdInput.appendChild(input)
+
+    //  show price
+    let tdPrice = document.createElement('td');
+    tdPrice.textContent = dataStore.price
+
+    // create button delete
+    let tdDelete = document.createElement('td');
+    let img = document.createElement('img');
+    img.src = "../../IMG/image/remove.png";
+
+    tdDelete.appendChild(img)
+    tableTR.appendChild(tdID);
+    tableTR.appendChild(tdName);
+    tableTR.appendChild(tdInput);
+    tableTR.appendChild(tdPrice);
+    tableTR.appendChild(tdDelete);
+
+    tbody.appendChild(tableTR)
+}
+
 
 // Call to data
 getDataLocalStorage();
 
 // Add action to element
 searchId.addEventListener('input', showProduct);
+
+//pash product
+add.addEventListener('click', showOnProduct)
